c6f187b19d11a7588a4e81f7b9c78503b75b4a17
Victor Basso <vitobasso@gmail.com>
2017-12-19 21:53:18 +0100

    recover UdpSocket. polishing.

--- src/main/scala/com/dominikgruber/scalatorrent/dht/UdpSocket.scala
+++ src/main/scala/com/dominikgruber/scalatorrent/dht/UdpSocket.scala
@@ -3,7 +3,7 @@ package com.dominikgruber.scalatorrent.dht
 import java.net.InetSocketAddress
 import java.nio.charset.StandardCharsets.ISO_8859_1
 
-import akka.actor.{Actor, ActorLogging, ActorRef}
+import akka.actor.{Actor, ActorLogging, ActorRef, Terminated}
 import akka.io.Udp._
 import akka.io.{IO, Udp}
 import akka.util.ByteString
@@ -19,40 +19,57 @@ object UdpSocket {
 
 case class UdpSocket(listener: ActorRef, port: Int) extends Actor with ActorLogging {
 
+  //TODO tolerate akka://scala-torrent/system/IO-UDP-FF/selectors/$a/0 - Can't assign requested address
   val udpManager: ActorRef = IO(Udp)(context.system)
 
   override def preStart(): Unit = {
-    udpManager ! Bind(self, new InetSocketAddress("0.0.0.0", port))
+    bind()
   }
 
-  def receive: Receive = {
+  override def receive: Receive = binding
+
+  def binding: Receive = {
     case Bound(local) =>
-      context.become(ready(sender()))
+      val socket = sender()
+      context.watch(socket)
+      context.become(ready(socket))
     case other =>
   }
 
   def ready(socket: ActorRef): Receive = {
     case Received(data, remote) =>
-      val str = data.decodeString(ISO_8859_1)
-      KrpcEncoding.decode(str) match {
-        case Right(msg) =>
-          log.debug(s"Received: ${show(msg)}")
-          listener ! ReceivedFromNode(msg, remote)
-        case Left(err) =>
-          log.error(s"Failed to parse message: $err. $str")
-      }
-
+      receive(data, remote)
     case SendToNode(msg, remote) =>
-      KrpcEncoding.encode(msg) match {
-        case Right(str) =>
-          log.debug(s"Sending: ${show(msg)}")
-          socket ! Send(ByteString(str, ISO_8859_1), remote) //TODO a test requiring the charset
-        case Left(err) =>
-          log.error(s"Failed to send message: $err")
-      }
-
+      send(socket, msg, remote)
     case Unbind => socket ! Unbind
     case Unbound => context.stop(self)
+    case Terminated(actor) =>
+      log.warning(s"Trying to reconnect to recover from failure in ${actor.path}")
+      context.become(binding)
+      bind()
   }
+
+  def receive(data: ByteString, remote: InetSocketAddress): Unit = {
+    val str = data.decodeString(ISO_8859_1)
+    KrpcEncoding.decode(str) match {
+      case Right(msg) =>
+        log.debug(s"Received: ${show(msg)}")
+        listener ! ReceivedFromNode(msg, remote)
+      case Left(err) =>
+        log.error(s"Failed to parse message: $err. $str")
+    }
+  }
+
+  def send(socket: ActorRef, message: Message, remote: InetSocketAddress): Unit = {
+    KrpcEncoding.encode(message) match {
+      case Right(str) =>
+        log.debug(s"Sending: ${show(message)}")
+        socket ! Send(ByteString(str, ISO_8859_1), remote) //TODO a test requiring the charset
+      case Left(err) =>
+        log.error(s"Failed to send message: $err")
+    }
+  }
+
+  def bind(): Unit = udpManager ! Bind(self, new InetSocketAddress("0.0.0.0", port))
 
 }
